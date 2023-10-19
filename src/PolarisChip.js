fb43700f66bc2f06732b2784ce5347ca66cddc21
evanb5150 <evanb5150@gmail.com>
2023-10-19 12:46:41 -0400

    j

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -21,6 +21,26 @@ export class PolarisChip extends LitElement {
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`<div class="news-card-one">
+    <div class="card-content">
+    <h1>News</h1>
+    <img hr="https://hr.psu.edu/sites/hr/files/styles/article_home_page/public/2023-10/FluVaccines_HRFeature.jpg?h=09bc788e&itok=KEMIew2T"> 
+    <div class="date-card">
+      <!-- <span> class="month"</span>
+      <span> class="day"</span> -->
+    </div>
+    <p1></p1>
+    </div>
+    </div>
+
+    <div class="news-card-two">
+    <div class="card-content">
+    <h1>News</h1>
+    <img hr="https://hr.psu.edu/sites/hr/files/styles/article_home_page/public/2023-10/ExpandedFamilySick_0.jpg?h=5265ff56&itok=e9egM1X8">
+    <div class="date-card">
+
+    </div>
+    </div>
+    </div>`;
   }
 }
