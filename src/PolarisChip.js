eeb98db63c3945fe898704a4d2d21225956b7d85
evanb5150 <evanb5150@gmail.com>
2023-10-22 19:16:55 -0400

    Update PolarisChip.js

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -24,7 +24,7 @@ export class PolarisChip extends LitElement {
     return html`<div class="news-card-one">
     <div class="card-content">
     <h1>News</h1>
-    <img hr="https://hr.psu.edu/sites/hr/files/styles/article_home_page/public/2023-10/FluVaccines_HRFeature.jpg?h=09bc788e&itok=KEMIew2T"> 
+    <img src="https://hr.psu.edu/sites/hr/files/styles/article_home_page/public/2023-10/FluVaccines_HRFeature.jpg?h=09bc788e&itok=KEMIew2T"> 
     <div class="date-card">
       <!-- <span> class="month"</span>
       <span> class="day"</span> -->
@@ -36,7 +36,7 @@ export class PolarisChip extends LitElement {
     <div class="news-card-two">
     <div class="card-content">
     <h1>News</h1>
-    <img hr="https://hr.psu.edu/sites/hr/files/styles/article_home_page/public/2023-10/ExpandedFamilySick_0.jpg?h=5265ff56&itok=e9egM1X8">
+    <img src="https://hr.psu.edu/sites/hr/files/styles/article_home_page/public/2023-10/ExpandedFamilySick_0.jpg?h=5265ff56&itok=e9egM1X8">
     <div class="date-card">
 
     </div>
